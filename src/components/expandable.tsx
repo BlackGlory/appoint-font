9fe34ada5952c843778b79cce3dbe079c48f04ae
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-16 22:47:01 +0800

    refactor: improve readability

--- src/components/expandable.tsx
+++ src/components/expandable.tsx
@@ -2,25 +2,28 @@ import { useToggle } from 'extra-react-hooks'
 import { ChevronRightIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
 import classNames from 'classnames'
 
-interface ICollapsibleProps {
+interface IExpandableProps {
   label: string
   children: React.ReactNode
 }
 
-export function Expandable({ label, children }: ICollapsibleProps) {
-  const [collapsed, toggleCollapsed] = useToggle(true)
+export function Expandable({ label, children }: IExpandableProps) {
+  const [expanded, toggleExpanded] = useToggle(false)
 
   return (
     <div>
-      <div className='w-full flex items-center cursor-pointer' onClick={toggleCollapsed}>
+      <div
+        className='w-full flex items-center cursor-pointer'
+        onClick={toggleExpanded}
+      >
         {
-          collapsed
-          ? <ChevronRightIcon className='w-4 h-4'/>
-          : <ChevronDownIcon className='w-4 h-4' />
+          expanded
+          ? <ChevronDownIcon className='w-4 h-4' />
+          : <ChevronRightIcon className='w-4 h-4'/>
         }
         <span>{label}</span>
       </div>
-      <div className={classNames(collapsed && 'hidden')}>
+      <div className={classNames(!expanded && 'hidden')}>
         {children}
       </div>
     </div>
